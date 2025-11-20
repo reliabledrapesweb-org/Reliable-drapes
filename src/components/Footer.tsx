1dafda55df4fdc8b5643b5cb8bb3c5af5413d6e3
Kolade <akoladesalako5@gmail.com>
2025-11-20 14:46:07 +0100

    Fix: Fixed UI bug with the site logo.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,8 +30,8 @@ export function Footer() {
               <Image
                 src="/images/logo.png"
                 alt="Logo"
-                width={240}
-                height={76}
+                width={100}
+                height={100}
                 className="object-contain md:w-[130px] lg:w-[140px]"
                 priority
               />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,8 +38,8 @@ export function Header() {
               <Image
                 src={isHome ? "/images/logo.png" : "/images/defaultlogo.png"}
                 alt="Logo"
-                width={240}
-                height={76}
+                width={100}
+                height={100}
                 className="object-contain md:w-[130px] lg:w-[140px]"
                 priority
               />
