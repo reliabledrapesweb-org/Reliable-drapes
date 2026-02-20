6b6dfe3b8d528d5452570aee6ac863519e79c658
Kolade <akoladesalako5@gmail.com>
2026-02-20 13:59:10 +0100

    Reorder header links: move Shop after Store Locator

--- src/lib/constants/navigation.ts
+++ src/lib/constants/navigation.ts
@@ -6,10 +6,10 @@ import type { NavLink } from "@/lib/types";
 
 export const NAV_LINKS: NavLink[] = [
   { name: "The Brand", link: "/about" },
-  { name: "Shop", link: "/shop" },
   { name: "E-catalogue", link: "/e-catalogue" },
   { name: "Exhibitions & Moments", link: "/exhibitions-events" },
   { name: "Store Locator", link: "/store-locator" },
+  { name: "Shop", link: "/shop" },
   { name: "Style Expert", link: "/style-expert" },
   { name: "Contact Us", link: "/contact" },
   { name: "Careers", link: "/careers" },
